d66f512c54140844ba282391cff31fb59e033d10
buzhidaoGH <2368244675@qq.com>
2021-06-05 23:07:41 +0800

    main更改

--- vue.config.js
+++ vue.config.js
@@ -14,4 +14,10 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '商城'
+      return args
+    })
+  },
 }
